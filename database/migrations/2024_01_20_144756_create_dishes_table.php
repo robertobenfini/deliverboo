c0df59fd2cfb15a97cd1320b01734842718dcb91
Paolo Falco <paolo.falcoapp@gmail.com>
2024-01-20 19:39:03 +0100

    add dishes migration

---
+++ database/migrations/2024_01_20_144756_create_dishes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('restaurant_id');
+            $table->string('name', 40);
+            $table->text('description', 5000);
+            $table->string('ingredients', 500);
+            $table->tinyInteger('visible');
+            $table->decimal('price', 10,2);
+            $table->timestamps();
+
+            // Aggiungi la chiave esterna
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
