58bf3574ae3ddde6e4683015e838ed3e6d56e820
Paolo Falco <paolo.falcoapp@gmail.com>
2024-01-20 19:39:15 +0100

    add order migration

---
+++ database/migrations/2024_01_20_144812_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 40);
+            $table->string('lastname', 40);
+            $table->string('address', 50);
+            $table->string('phone', 40);
+            $table->tinyInteger('status');
+            $table->decimal('totalprice', 10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
