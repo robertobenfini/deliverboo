acfac24ea7454b4afe0b8230fd52bc2edd4f8a59
robertobenfini <roberto.benfini@gmail.com>
2024-01-24 17:19:52 +0100

    correzione-errore

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -2,49 +2,48 @@
 
 @section('content')
     <form 
-    method="POST" 
-    action="{{ route('admin.restaurants.store') }}" 
-    enctype="multipart/form-data"  
-    novalidate
-    class="container-fluid d-flex flex-column"
+        method="POST" 
+        action="{{ route('admin.restaurants.store') }}" 
+        enctype="multipart/form-data"  
+        novalidate
+        class="container-fluid d-flex flex-column"
     >
-        {{-- Per protezione dati --}}
+        {{-- Protezione dati --}}
         @csrf 
-        {{-- Per protezione dati --}}
+        {{-- Protezione dati --}}
 
-    <div class="container d-flex p-4">
-        <fieldset class="d-flex flex-column">
-            <div class="">
-                <p class="font-medium">Informations</p>
-                <p class="text-xs">Put the information here to create a new restaurant</p>
-            </div>
-            <div class="d-flex flex-column">
-                <label for="">Name</label>
-                <input type="text" class="rounded @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
-                <div class="invalid-feedback">
-                    @error('name') {{ $message }} @enderror
+        <div class="container d-flex p-4">
+            <fieldset class="d-flex flex-column">
+                <div>
+                    <p class="font-medium">Informations</p>
+                    <p class="text-xs">Put the information here to create a new restaurant</p>
                 </div>
 
-                <label for="">Address</label>
-                <input type="text" class="rounded @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address') }}">
-                <div class="invalid-feedback">
-                    @error('address') {{ $message }} @enderror
+                <div class="d-flex flex-column">
+                    <label for="name">Name</label>
+                    <input type="text" class="rounded @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
+                    <div class="invalid-feedback">
+                        @error('name') {{ $message }} @enderror
+                    </div>
+
+                    <label for="address">Address</label>
+                    <input type="text" class="rounded @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address') }}">
+                    <div class="invalid-feedback">
+                        @error('address') {{ $message }} @enderror
+                    </div>
+
+                    <label for="p_iva">P.Iva</label>
+                    <input type="text" class="rounded @error('p_iva') is-invalid @enderror" id="p_iva" name="p_iva" value="{{ old('p_iva') }}">
+                    <div class="invalid-feedback">
+                        @error('p_iva') {{ $message }} @enderror
+                    </div>
+
+                    <label for="photo">Photo</label>
+                    <input type="text" class="rounded @error('photo') is-invalid @enderror" id="photo" name="photo" value="{{ old('photo') }}">
                 </div>
 
-                <label for="">P.Iva</label>
-                <input type="text" class="rounded @error('p_iva') is-invalid @enderror" id="p_iva" name="p_iva" value="{{ old('p_iva') }}">
-                <div class="invalid-feedback">
-                    @error('p_iva') {{ $message }} @enderror
-                </div>
-
-                <label for="">Photo</label>
-                <input type="text" class="rounded @error('photo') is-invalid @enderror" id="photo" name="photo" value="{{ old('photo') }}">
-            </div>
-            <div class="col-span-full sm:col-span-6">
-                <div class="col-span-full sm:col-span-6">
-                    <label for="typology" class="form-label"style="font-weight:700; font-size:20px">
-                        Typologies
-                    </label>
+                <div class="col-span-full sm:col-span-6 mt-3">
+                    <label for="typology" class="form-label" style="font-weight: 700; font-size: 20px">Typologies</label>
                     @foreach($typologies as $typology)
                         <div class="mb-3 form-check">
                             <input
@@ -56,30 +55,31 @@
                                 @if (in_array($typology->id, old('typologies') ?: [])) checked @endif
                             >
                             <label class="form-check-label" for="typology{{ $typology->id }}">{{ $typology->name }}</label>
-                            
                         </div>
-            <div class="col-span-full sm:col-span-6 mt-3">
-                <label for="typology" class="form-label"style="font-weight:700; font-size:20px">
-                    Typologies
-                </label>
-                <select 
-                class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900 form-select 
-                @error('typology_id') is-invalid @enderror" 
-                id="typology" 
-                name="typology_id">
-                    <option selected>
-                        Change typology
-                    </option>
-    
-                    @foreach ($typologies as $typology)
-                        <option value="{{ $typology->id }}">{{ $typology->name }}</option>
                     @endforeach
                 </div>
-            </div>
-            <div class="mt-4 d-flex justify-content-center">
-                <button class="btn btn-primary mt-4">Create</button>
-            </div>
-        </fieldset>
-    </div>
+
+                <div class="col-span-full sm:col-span-6 mt-3">
+                    <label for="typology" class="form-label" style="font-weight: 700; font-size: 20px">Typology</label>
+                    <select 
+                        class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900 form-select @error('typology_id') is-invalid @enderror" 
+                        id="typology" 
+                        name="typology_id"
+                    >
+                        <option selected>
+                            Change typology
+                        </option>
+    
+                        @foreach ($typologies as $typology)
+                            <option value="{{ $typology->id }}">{{ $typology->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mt-4 d-flex justify-content-center">
+                    <button class="btn btn-primary mt-4">Create</button>
+                </div>
+            </fieldset>
+        </div>
     </form>
 @endsection
