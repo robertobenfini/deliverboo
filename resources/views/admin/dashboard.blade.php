1e71e123b9236cc9b5f2bad1ee44e3f7275f7c64
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2024-01-25 13:04:56 +0100

    add style dashboard - create

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,28 +1,53 @@
-@extends('layouts.admin')
+@extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    
+        <div class="db-container">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card p-4">
+                            
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                            <div class="card-body text-center">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                                {{-- <div class="row">
+                                    <div class="col-6">
+                                        Nome Utente
+                                        FOto
+                                    </div>
+                                    <div class="col-6">
+                                        I tuoi Guadagni
+                                    </div>
+                                </div> --}}
+                                {{-- <div class="row">
+                                    <div class="col-6">
+                                        Vai ai ristoranti
+                                    </div>
+                                    <div class="col-6">
+                                        Statistiche
+                                    </div>
+                                </div> --}}
+                                <h2>
+                                    Benvenuto <span class="font-weight-bolder">{{ Auth::user()->name }}</span> {{ __('nella tua area personale!') }}
+                                </h2>
+                                <h5 class="mt-4">Grazie di far parte della nostra rete dal <strong>{{ date_format(Auth::user()->created_at, 'd/m/Y') }}</strong>!</h5>
+                            </div>
+                            
+                            <div class="p-4 gs-card-text">
+                                <span><i class="fa-solid fa-right-long"></i></span>
+                                <span><a href="{{ route('admin.restaurants.index') }}" class="gs-link">I tuoi ristoranti</a></span>
+                            </div>
+                            
+                        </div>
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-
-                    <h1>ciao</h1>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    
+    
 @endsection
