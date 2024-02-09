57fc0372f91ca5952af0d16ebf9c2ccc36cc3c51
matteo2302 <matteo.pizzichemi01@gmail.com>
2024-02-09 14:06:19 +0100

    style welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
                 <h1>Ristoranti</h1>
                 <p>
                     Alla consegna ci pensiamo noi, così tu potrai concentrarti sul cibo.</p>
-                <a class="link-know">Scopri di più</a>
+                <a>Scopri di più</a>
             </div>
         </div>
 
@@ -20,7 +20,7 @@
             <div class="know-more supermarket d-flex  flex-column justify-content-end">
                 <h1>Supermercati</h1>
                 <p>Ti aiuteremo a raggiungere nuovi clienti online per incrementare le vendite.</p>
-                <a class="link-know">Scopri di più</a>
+                <a>Scopri di più</a>
             </div>
         </div>
 
@@ -38,6 +38,79 @@
                 <li>Fidelizza la clientela grazie ai dati e agli strumenti disponibili nel Restaurant Hub</li>
             </ul>
         </div>
-        <div class="rider-spalle-img"><img src="../../public/img/rider-spalle.jpg" alt="rider">lalala</div>
+        <div class="rider-spalle-img"><img src="./../../img/rider-spalle.jpg" alt="rider"></div>
+    </div>
+    <div class="improves d-flex flex-column justify-content-center align-items-center">
+        <h1>Modi per far crescere e supportare la tua attività</h1>
+        <p>Prodotti, servizi e informazioni per il tuo successo.</p>
+        <div class="box d-flex justify-content-between align-items-center">
+
+            <div>
+                <div class="improves-marketing marketing d-flex  flex-column justify-content-end">
+                    <h1>Marketing</h1>
+                    <p>Raggiungi nuovi clienti a livello locale con le nostre iniziative di marketing e pubblicizza la tua
+                        attività con pochi clic</p>
+                    <a>Scopri di più</a>
+                </div>
+            </div>
+
+            <div>
+                <div class="improves-consegna consegna d-flex  flex-column justify-content-end">
+                    <h1>Consegna</h1>
+                    <p>Offri il servizio di consegna per garantire ai tuoi clienti la migliore esperienza possibile.</p>
+                    <a>Scopri di più</a>
+                </div>
+            </div>
+            <div>
+                <div class="improves-ritiri ritiri d-flex  flex-column justify-content-end">
+                    <h1>Ritiri</h1>
+                    <p>Dai ai clienti la possibilità di ritirare l'ordine presso il tuo ristorante.</p>
+                    <a>Scopri di più</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer d-flex align-items-center">
+        <div class="box d-flex justify-content-between ">
+            <div class="footer-card">
+                <div class=" f-restaurant">
+                    <h1>Restaurant</h1>
+                    <ul>
+                        <li><a href="#">Signature</a></li>
+                        <li><a href="#">La nostra tecnoligia</a></li>
+                        <li><a href="#">Marketing</a></li>
+                        <li><a href="#">Food Photography</a></li>
+                        <li><a href="#">Integrazione del Sistema di Cassa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-card ">
+                <div class="legale">
+                    <h1>Legale</h1>
+                    <ul>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Cookie</a></li>
+                        <li><a href="#">Termini & condizioni</a></li>
+                        <li><a href="#">Politiche</a></li>
+                        <li><a href="#">Richieste da parte delle Autorità Pubbliche</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-card">
+                <div class=" help">
+                    <h1>aiuto</h1>
+                    <ul>
+                        <li><a href="#">Consigli e risposte dal team</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-card">
+                <div class="partner">
+                    <h1>Partner</h1>
+                    <p>Inviaci le tue informazioni e ti aiuteremo a diventare un partner di Deliveroo.</p>
+                    <a class="footer-btn">Scopri di più</a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
