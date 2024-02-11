d7a8e1ed90a1b0bbae4e3b6f07b743190f90147c
Paolo Falco <paolo.falcoapp@gmail.com>
2024-02-11 21:53:02 +0100

    add order

--- config/orders.php
+++ config/orders.php
@@ -1,48 +1,48 @@
 <?php
 return [
-    // [
-    //     'name' => 'Elisabetta',
-    //     'lastname' => 'Fresi',
-    //     'address' => 'Via Roma 32, Frosinone',
-    //     'phone' => '3281234567',
-    //     'status' => true,
-    //     'totalprice' => 8,
-    //     'dishes' => [1]
-    // ],
-    // [
-    //     'name' => 'Francesco',
-    //     'lastname' => 'Marzotti',
-    //     'address' => 'Via Milano 54, Chieti',
-    //     'phone' => '3287654321',
-    //     'status' => true,
-    //     'totalprice' => 8,
-    //     'dishes' => [1]
-    // ],
-    // [
-    //     'name' => 'Giovanni',
-    //     'lastname' => 'Rossi',
-    //     'address' => 'Via Garibaldi 10, Roma',
-    //     'phone' => '3339876543',
-    //     'status' => true,
-    //     'totalprice' => 12,
-    //     'dishes' => [1, 2, 3] 
-    // ],
-    // [
-    //     'name' => 'Laura',
-    //     'lastname' => 'Bianchi',
-    //     'address' => 'Corso Italia 20, Milano',
-    //     'phone' => '3451234567',
-    //     'status' => true,
-    //     'totalprice' => 15,
-    //     'dishes' => [1, 2, 4] 
-    // ],
-    // [
-    //     'name' => 'Mario',
-    //     'lastname' => 'Verdi',
-    //     'address' => 'Via Dante 5, Firenze',
-    //     'phone' => '3478765432',
-    //     'status' => true,
-    //     'totalprice' => 10,
-    //     'dishes' => [1, 3] 
-    // ]
+    [
+        'name' => 'Elisabetta',
+        'lastname' => 'Fresi',
+        'address' => 'Via Roma 32, Frosinone',
+        'phone' => '3281234567',
+        'status' => true,
+        'totalprice' => 8,
+        'dishes' => [1]
+    ],
+    [
+        'name' => 'Francesco',
+        'lastname' => 'Marzotti',
+        'address' => 'Via Milano 54, Chieti',
+        'phone' => '3287654321',
+        'status' => true,
+        'totalprice' => 8,
+        'dishes' => [1]
+    ],
+    [
+        'name' => 'Giovanni',
+        'lastname' => 'Rossi',
+        'address' => 'Via Garibaldi 10, Roma',
+        'phone' => '3339876543',
+        'status' => true,
+        'totalprice' => 12,
+        'dishes' => [1, 2, 3] 
+    ],
+    [
+        'name' => 'Laura',
+        'lastname' => 'Bianchi',
+        'address' => 'Corso Italia 20, Milano',
+        'phone' => '3451234567',
+        'status' => true,
+        'totalprice' => 15,
+        'dishes' => [1, 2, 4] 
+    ],
+    [
+        'name' => 'Mario',
+        'lastname' => 'Verdi',
+        'address' => 'Via Dante 5, Firenze',
+        'phone' => '3478765432',
+        'status' => true,
+        'totalprice' => 10,
+        'dishes' => [1, 3] 
+    ]
 ];
